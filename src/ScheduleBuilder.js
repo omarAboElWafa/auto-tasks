40ea024b8f83e843c15988b13aa7abcf1989786a
Omar A. H. Abo El Wafa <58788836+omarAboElWafa@users.noreply.github.com>
2024-10-04 21:25:17 +0300

    refactor: Update ScheduleBuilder.js to include time unit validation and execution chain tracking

--- src/ScheduleBuilder.js
+++ src/ScheduleBuilder.js
@@ -1,7 +1,10 @@
 const ScheduleFactory = require("./ScheduleFactory");
+const { validateTimeFormat } = require("./utils/validators");
+
 class ScheduleBuilder {
   constructor() {
     this.scheduleData = {};
+    this._excutionChain = [];
   }
 
   every(timeUnit = "week") {
@@ -15,10 +18,14 @@ class ScheduleBuilder {
       );
     }
     this.scheduleData.every = timeUnit.toLowerCase();
+    this._excutionChain.push("every");
     return this;
   }
 
-  at(time) {
+  at(time = "00:00") {
+    if (!this._excutionChain.includes("every")) {
+      this.every();
+    }
     // valid only for every week || every day
     if (
       this.scheduleData.every !== "week" &&
@@ -26,19 +33,36 @@ class ScheduleBuilder {
     ) {
       throw new Error("Invalid time unit, every must be set to week or day");
     }
+    if (typeof time === "number") {
+      time = time.toString();
+    }
+
+    if (!validateTimeFormat(time)) {
+      throw new Error("Invalid time format");
+    }
     this.scheduleData.at = time;
+    this._excutionChain.push("at");
     return this;
   }
 
   on(day) {
+    if (!this._excutionChain.includes("every")) {
+      this.every();
+    }
     if (this.scheduleData.every !== "week") {
       throw new Error("Invalid time unit, every must be set to week");
     }
     this.scheduleData.on = day;
+    this._excutionChain.push("on");
     return this;
   }
 
   period(period) {
+    if (!this._excutionChain.includes("at")) {
+      throw new Error(
+        "Invalid time format, 'at' must be set before calling 'period'"
+      );
+    }
     if (
       this.scheduleData.every !== "week" ||
       this.scheduleData.every !== "day"
@@ -50,6 +74,7 @@ class ScheduleBuilder {
       throw new Error("Invalid period");
     }
     this.scheduleData.period = period.toLowerCase();
+    this._excutionChain.push("period");
     return this;
   }
 
