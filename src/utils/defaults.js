b0ee36a21c606af5a2bd74785cd4b1e2458423e7
Omar A. H. Abo El Wafa <58788836+omarAboElWafa@users.noreply.github.com>
2024-10-05 11:53:27 +0300

    feat: Add defaults and validTimeUnits to utils/defaults.js

---
+++ src/utils/defaults.js
@@ -0,0 +1,13 @@
+const DEAFULTS = {
+  every: "week",
+  on: "monday",
+  at: "00:00",
+  period: "am",
+};
+
+const validTimeUnits = ["day", "week", "hour"];
+
+module.exports = {
+  DEAFULTS,
+  validTimeUnits,
+};
