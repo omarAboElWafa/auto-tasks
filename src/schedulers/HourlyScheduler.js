9dde0030132970e82230f89d9d207bfcb9ceaac4
Omar A. H. Abo El Wafa <58788836+omarAboElWafa@users.noreply.github.com>
2024-10-06 23:23:59 +0300

    refactor: Update HourlyScheduler.js to include time period validation and default period value

--- src/schedulers/HourlyScheduler.js
+++ src/schedulers/HourlyScheduler.js
@@ -1,18 +1,24 @@
 const Scheduler = require("../Scheduler.js");
+const { validateWeekDays } = require("../utils/validators.js");
 
 class HourlyScheduler extends Scheduler {
   constructor(day) {
     super();
-    if (this.day) {
-      this.day = day.toLowerCase(); // tuesday
+    this.everyMinute = day ? false : true;
+    this.day = validateWeekDays(day);
+    if (!this.day && !this.everyMinute) {
+      throw new Error("Invalid day");
     }
   }
 
   toCronExp() {
     if (this.day) {
-      return `${this.day} * * *`;
+      return `0 * * * ${this.day - 1}`;
     }
-    return "* * * * *";
+    if (this.everyMinute) {
+      return "* * * * *";
+    }
+    throw new Error("Invalid Cron Operation");
   }
 }
 
