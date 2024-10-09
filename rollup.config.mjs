863df436ff1b8b654f04d6e871cc9dfa1da058bc
Omar A. H. Abo El Wafa <58788836+omarAboElWafa@users.noreply.github.com>
2024-10-09 20:53:43 +0300

    refactor: Update .gitignore and package.json
    - Remove unused '/tests' entry from .gitignore
    - Add '.npmignore' to .gitignore
    - Update package.json version to 0.0.4
    - Update package-lock.json version to 0.0.4
    - Remove 'types' entry from package.json
    - Add 'files' entry to package.json
    - Update rollup.config.mjs to output both CommonJS and ES modules

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,13 +1,19 @@
-import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
 import json from "@rollup/plugin-json";
 import terser from "@rollup/plugin-terser";
 
 export default {
   input: "src/index.js",
-  output: {
-    file: "dist/bundle.js",
-    format: "cjs",
-  },
+  output: [
+    {
+      file: "dist/index.js",
+      format: "cjs",
+    },
+    {
+      file: "dist/index.mjs",
+      format: "es",
+    },
+  ],
   plugins: [resolve(), commonjs(), json(), terser()],
 };
